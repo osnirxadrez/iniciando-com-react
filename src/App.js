b63c8a30568afd9ec8ba5c2e5ec8809134b00906
Osnir Basílio <osnirxadrez@gmail.com>
2021-07-10 11:53:06 -0300

    Comandos basicos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    nome: ''
+  }
+
+  criaComboBox = () => {
+    const opcoes = ["José","Osnir"]
+    const comboBoxOpcoes = opcoes.map(opcao => <option> {opcao} </option>)
+    return(
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
+  modificarNome = (event) => {
+       this.setState({
+         nome: event.target.value
+       })
+  }
+
+  render(){
+    const MeuComboBox = () => this.criaComboBox();
+
+    return(
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello {this.props.nome} sua idade é {this.props.idade} </h1>
+        <MeuComboBox/>
+      </>
+    )
+  }
 }
 
 export default App;
